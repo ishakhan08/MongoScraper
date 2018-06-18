98cbda7bbcf17ee312ed0ffa2bddf5c051737da5
Isha Khan <khan.isha08@gmail.com>
2018-06-17 21:54:17 -0500

    started

---
+++ models/Note.js
@@ -0,0 +1,18 @@
+// Require mongoose
+var mongoose = require("mongoose");
+// Create a schema class
+var Schema = mongoose.Schema;
+
+
+var NoteSchema = new Schema({
+    body: {
+        type: String
+    },
+    article: {
+        type: Schema.Types.ObjectId,
+        ref: "Article"
+    }
+});
+var Note = mongoose.model("Note", NoteSchema);
+
+module.exports = Note;
